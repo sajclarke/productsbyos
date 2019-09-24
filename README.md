621688934c0beed01fa59f2f2f9cb48a6377e199
Shannon Clarke <shannonajclarke@gmail.com>
2019-09-24 00:52:29 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# productsbyos website
---
+++ gulpfile.js
@@ -0,0 +1,113 @@
+// Load plugins
+const browsersync = require('browser-sync').create();
+const gulp = require('gulp');
+var nunjucksRender = require('gulp-nunjucks-render');
+var data = require('gulp-data');
+var path = require('path');
+var fs = require('fs');
+var netlify = require('gulp-netlify');
+var merge = require('gulp-merge-json');
+
+var git = require('gulp-git');
+
+const imagemin = require('gulp-imagemin');
+
+var manageEnvironment = function(env) {
+  env.addFilter('split', function(str, seperator) {
+    return str.split(seperator);
+  });
+};
+
+gulp.task('render_content', function(cb) {
+  // Copy assets to dist folder
+  gulp.src(['./assets/**/*']).pipe(gulp.dest('./build/assets/'));
+  gulp.src(['pages/**/*.yml']).pipe(gulp.dest('./build/'));
+
+  //Render images
+  // gulp
+  //   .src("./assets/images/**/*")
+  //   .pipe(imagemin())
+  //   .pipe(gulp.dest("dist/assets/images"));
+
+  //Render nunjucks to html
+  gulp
+    .src('pages/**/*.+(html|njk)')
+    // Adding data to Nunjucks
+
+    .pipe(
+      data(function() {
+        var arr = [];
+        var folder = './posts';
+
+        var files = fs.readdirSync(folder);
+
+        files.forEach(file => {
+          let fileStat = fs.statSync(folder + '/' + file).isDirectory();
+          if (!fileStat) {
+            if (file.includes('json')) {
+              arr.push(JSON.parse(fs.readFileSync('./posts/' + file)));
+            }
+          }
+        });
+
+        var contents = { articles: arr };
+        return contents;
+      })
+    )
+    .pipe(
+      nunjucksRender({
+        path: ['templates/'], // String or Array
+        data: {
+          site_title: 'Os Foods',
+          instagram_username: '',
+          contact_form_url: 'https://docs.google.com/forms/d/1DOxT0tkNRE35pucLyZMH_eght_Enhr0RcofOX-8C3xs/formResponse'
+        },
+        envOptions: {
+          watch: false
+        },
+        manageEnv: manageEnvironment
+      })
+    )
+    .pipe(gulp.dest('build'));
+  cb();
+});
+
+gulp.task('render_images', function(cb) {
+  gulp
+    .src('./assets/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('build/assets/images'));
+  cb();
+});
+
+// BrowserSync
+function browserSync(done) {
+  browsersync.init({
+    server: {
+      baseDir: './build'
+    }
+  });
+  done();
+}
+
+// BrowserSync Reload
+function browserSyncReload(done) {
+  // gulp.parallel("vendor");
+  browsersync.reload();
+  done();
+}
+
+// Watch files
+function watchFiles(done) {
+  gulp.watch('./posts/**/*', gulp.series('render_content'));
+  gulp.watch('./assets/**/*', gulp.series('render_content'));
+  gulp.watch('./templates/**/*', gulp.series('render_content'));
+  gulp.watch('./pages/**/*', gulp.series('render_content'));
+  gulp.watch('./build/*.html', browserSyncReload);
+  done();
+}
+
+gulp.task('default', gulp.parallel('render_content', 'render_images'));
+
+// dev task
+gulp.task('dev', gulp.parallel('render_content', watchFiles, browserSync));
